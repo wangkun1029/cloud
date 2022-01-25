9c8d240b798e272af62334b17df2d74415b79e55
wangkun <kun.wang2@56pingtai.net>
2022-01-25 11:05:17 +0800

    gateway的完善

---
+++ cloud-config-center/src/main/java/com/tdh/springcloud/ConfigCenterApplication.java
@@ -0,0 +1,14 @@
+﻿package com.tdh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApplication.class,args);
+    }
+}
